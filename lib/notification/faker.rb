2f2ae1936bf45f903f26da1eb6bb6b0593eba0e1
Tzer Wong <tzer@mit.edu>
2016-06-10 17:09:30 -0400

    added class

--- lib/notification/faker.rb
+++ lib/notification/faker.rb
@@ -2,6 +2,65 @@ require "notification/faker/version"
 
 module Notification
   module Faker
-    # Your code goes here...
+  class NotificationFaker
+    def initialize
+      @member_id = Faker::Bitcoin.address
+      @app_name = Faker::Lorem.word
+      @short_title = Faker::Lorem.word
+      @short_body = Faker::Lorem.sentence
+      @device_id = Faker::Code.ean
+    end
+
+    def member_id
+      @member_id
+    end
+
+    def app_name
+      @app_name
+    end
+
+    def short_title
+      @short_title
+    end
+
+    def short_body
+      @short_body
+    end
+
+    def device_id
+      @device_id
+    end
+
+    def new_notification
+      params = {
+              'member_id' => @member_id,
+	      'app_name' => @app_name,
+              'short_title' => @short_title,
+	      'short_body' => @short_body
+             }
+    end
+
+    def new_registration
+      params = {
+              'member_id' => @member_id,
+              'app_name' => @app_name,
+              'device_id'=> @device_id
+      }
+    end
+
+    def get_notification
+      {'member_id' => @member_id, 'app_name' => @app_name}
+    end
+
+    def get_subscription
+      {'member_id' => @member_id, 'app_name' => @app_name}
+    end
+
+    def upsert_subscription
+      params = {
+              'member_id' => @member_id,
+              'app_name' => @app_name,
+              'channels' => 'push'
+      }
+    end
   end
-end
