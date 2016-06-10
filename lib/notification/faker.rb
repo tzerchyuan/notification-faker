b2fd445b0b84741d3b9c01a7d9ad9fbadb4d865c
tzerchyuan <tzerchyuan@users.noreply.github.com>
2016-06-10 17:12:50 -0400

    Update faker.rb

--- lib/notification/faker.rb
+++ lib/notification/faker.rb
@@ -2,65 +2,67 @@ require "notification/faker/version"
 
 module Notification
   module Faker
-  class NotificationFaker
-    def initialize
-      @member_id = Faker::Bitcoin.address
-      @app_name = Faker::Lorem.word
-      @short_title = Faker::Lorem.word
-      @short_body = Faker::Lorem.sentence
-      @device_id = Faker::Code.ean
-    end
+    class NotificationFaker
+      def initialize
+        @member_id = Faker::Bitcoin.address
+        @app_name = Faker::Lorem.word
+        @short_title = Faker::Lorem.word
+        @short_body = Faker::Lorem.sentence
+        @device_id = Faker::Code.ean
+      end
 
-    def member_id
-      @member_id
-    end
+      def member_id
+        @member_id
+      end
 
-    def app_name
-      @app_name
-    end
+      def app_name
+        @app_name
+      end
 
-    def short_title
-      @short_title
-    end
+      def short_title
+        @short_title
+      end
 
-    def short_body
-      @short_body
-    end
+      def short_body
+        @short_body
+      end
 
-    def device_id
-      @device_id
-    end
+      def device_id
+        @device_id
+      end
 
-    def new_notification
-      params = {
+      def new_notification
+        params = {
               'member_id' => @member_id,
 	      'app_name' => @app_name,
               'short_title' => @short_title,
 	      'short_body' => @short_body
-             }
-    end
+        }
+      end
 
-    def new_registration
-      params = {
+      def new_registration
+        params = {
               'member_id' => @member_id,
               'app_name' => @app_name,
               'device_id'=> @device_id
-      }
-    end
+        }
+      end
 
-    def get_notification
-      {'member_id' => @member_id, 'app_name' => @app_name}
-    end
+      def get_notification
+        {'member_id' => @member_id, 'app_name' => @app_name}
+      end
 
-    def get_subscription
-      {'member_id' => @member_id, 'app_name' => @app_name}
-    end
+      def get_subscription
+        {'member_id' => @member_id, 'app_name' => @app_name}
+      end
 
-    def upsert_subscription
-      params = {
+      def upsert_subscription
+        params = {
               'member_id' => @member_id,
               'app_name' => @app_name,
               'channels' => 'push'
-      }
+        }
+      end
     end
   end
+end
